1f8d4e7a503d5f77b06dfc62788bb4024c2069cd
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2023-11-24 22:38:45 +0000

    ✅ nó armazena nós pai, filho esquerdo e filho direito

--- No.java
+++ No.java
@@ -1,5 +1,8 @@
 public class No<TipoGenerico> {
     private TipoGenerico elemento;
+    private No<TipoGenerico> noPai;
+    private No<TipoGenerico> filhoEsquerdo;
+    private No<TipoGenerico> filhoDireito;
 
     public No(TipoGenerico elemento) {
         this.elemento = elemento;
