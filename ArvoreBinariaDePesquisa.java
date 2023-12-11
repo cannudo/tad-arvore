8fa4babd6a582aa56e2e2b50db634e44afcc47e4
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2023-12-11 17:20:28 +0000

    🚧 TODO: adicionar um filho na classe No no momento setNoPai()

--- ArvoreBinariaDePesquisa.java
+++ ArvoreBinariaDePesquisa.java
@@ -7,6 +7,10 @@ class ArvoreBinariaDePesquisa {
         return (no.getFilhoEsquerdo() == null) && (no.getFilhoDireito() == null);
     }
 
+    public boolean isInternal(No no) {
+        return (no.getFilhoEsquerdo() != null) || (no.getFilhoDireito() != null);
+    }
+
     public No parent(No no) {
         return no.getNoPai(); 
     }
@@ -17,8 +21,22 @@ class ArvoreBinariaDePesquisa {
         No<String> primeiroNo = new No<>();
         ArvoreBinariaDePesquisa arvore = new ArvoreBinariaDePesquisa();
         System.out.println("Primeiro nó criado é root? " + arvore.isRoot(primeiroNo));
+        System.out.println("Primeiro nó criado é interno? " + arvore.isInternal(primeiroNo));
+        System.out.println("Primeiro nó criado é externo? " + arvore.isExternal(primeiroNo));
         No<String> segundoNo = new No<>();
+        segundoNo.setNoPai(primeiroNo);
         System.out.println("Segundo nó criado tem o primeiro como pai. É root? " + arvore.isRoot(segundoNo));
-        System.out.println("Segundo nó criado realmente tem o primeiro como pai? " + (segundoNo.getNoPai().equals(primeiroNo))); // ❌ dando false e era pra dar true
+        
+        System.out.println();
+        System.out.println();
+        primeiroNo.status();
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Primeiro nó criado é interno? " + arvore.isInternal(primeiroNo)); // ❌ dando false e era pra dar true TODO: adicionar o filho na classe No
+        System.out.println("Primeiro nó criado é externo? " + arvore.isExternal(primeiroNo));
+        System.out.println("Segundo nó criado realmente tem o primeiro como pai? " + (segundoNo.getNoPai() == primeiroNo));
+        System.out.println("Segundo nó criado é interno? " + arvore.isInternal(segundoNo));
+        System.out.println("Segundo nó criado é externo? " + arvore.isExternal(segundoNo));
     }
 }
